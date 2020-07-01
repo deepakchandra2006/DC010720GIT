3ac3585f92a00d8de006a7b8da3b79f4e22a6288
deepakchandra2006 <dsony2006@gmail.com>
2020-07-01 21:42:58 +0530

    second code upload

--- src/FirstGit.java
+++ src/FirstGit.java
@@ -4,6 +4,8 @@ public class FirstGit {
 	public static void main(String[] args) {
 
 		System.out.println("This is first GIT repository programe");
+		System.out.println("This is first GIT repository programe1");
+		System.out.println("This is first GIT repository programe2");
 	}
 
 }
